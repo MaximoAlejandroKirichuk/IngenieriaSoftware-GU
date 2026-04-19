1e2ce9f57f429bce92eab006913887412166d3ac
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2026-04-19 00:10:16 -0300

    feat(login): mejorar lógica de autenticación y control de usuario activo

--- BLL/Excepciones/Login/UsuarioActivoActualmenteException.cs
+++ BLL/Excepciones/Login/UsuarioActivoActualmenteException.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace BLL.Excepciones.Login
 {
-    internal class UsuarioActivoActualmenteException
+    public class UsuarioActivoActualmenteException : Exception
     {
+        public override string Message => "El usuario ya inicio sesion en este dispositivo";
     }
 }
--- BLL/GestorUsuariosBLL.cs
+++ BLL/GestorUsuariosBLL.cs
@@ -9,6 +9,7 @@ using BLL.Interfaces;
 using DAL.interfaces;
 using DAL;
 using BLL.Excepciones.Login;
+using Microsoft.Win32;
 namespace BLL
 {
     public class GestorUsuarioBLL : IGestorUsuario
@@ -16,28 +17,53 @@ namespace BLL
         private readonly IUsuarioDAL _dal;
         private readonly IEncriptador _encriptador;
         private readonly ISessionManager _sessionManager;
-        public GestorUsuarioBLL(IUsuarioDAL dal, IEncriptador encriptador, ISessionManager sessionManager)
+        private readonly IBitacoraManager _bitacora;
+        private const int NumeroIntentos = 3;
+        public GestorUsuarioBLL(IUsuarioDAL dal, IEncriptador encriptador, ISessionManager sessionManager, IBitacoraManager bitacora)
         {
             _dal = dal;
             _encriptador = encriptador;
             _sessionManager = sessionManager;
+            _bitacora = bitacora;
         }
 
         public void Login(string email, string contrasena)
         {
-            string hash = _encriptador.HashContrasena(contrasena);
             var usuario = _dal.ObtenerPorMail(email);
-            
+
+            if (_sessionManager.UsuarioActivo != null)
+                throw new UsuarioActivoActualmenteException();
+
             if (usuario == null)
+            {
+                _bitacora.RegistrarEvento($"Intento de login fallido: {email} no existe", 2, "Seguridad", email);
                 throw new UsuarioNoExisteException();
-            
+            }
+            string hash = _encriptador.HashContrasena(contrasena);
+
             if (usuario.Contrasena != hash)
-            //TODO: falta agregar el contador de errores
-                throw new ContrasenaInvalidaException();
+            {
+                usuario.Intentos++;
+                if (usuario.Intentos >= NumeroIntentos)
+                {
+                    usuario.Bloqueado = true;
+                    _dal.BloquearUsuario(usuario);
+                    // REGISTRO DE BLOQUEO (Criticidad 1)
+                    _bitacora.RegistrarEvento($"Usuario bloqueado: {email}", 1, "Usuario", email);
+                    throw new UsuarioBloqueadoException();
+                }
+
+                _dal.ActualizarIntentos(usuario);
+                throw new ContrasenaInvalidaException($"Intento {usuario.Intentos} de 3.");
+            }
+
+            // LOGIN EXITOSO
             _sessionManager.IniciarSesion(usuario);
-
+            usuario.Intentos = 0; // Reseteamos intentos
+            _dal.ActualizarIntentos(usuario);
+            //REGISTRO DE LOGIN(Criticidad 1)
+            _bitacora.RegistrarEvento($"Login exitoso: {usuario.Email}", 1, "Usuario", email);
         }
-
         public void Logout()
         {
             throw new NotImplementedException();
