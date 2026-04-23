fc0fb37a75e0eb26b7054fbda43984928266a796
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2026-04-23 00:23:46 -0300

    feat(desbloquear-usuario/ui): agregar botón para desbloquear usuario desde DataGridView

--- UI/FrmGestionUsuarios.cs
+++ UI/FrmGestionUsuarios.cs
@@ -44,9 +44,9 @@ namespace UI
         {
             try
             {
-                Usuario_83KI usuarioElegido = dgvUsuarios.Rows[0].DataBoundItem; 
+                Usuario_83KI usuarioElegido = (Usuario_83KI) dgvUsuarios.Rows[0].DataBoundItem; 
                 _gestorUsuario.DesbloquearCuenta(usuarioElegido);
-
+                ActualizarDatos();
             }
             catch (Exception)
             {
