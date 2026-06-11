aa607b0de63c523ea285194465c05fefb80e903c
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2026-06-11 00:43:58 -0300

    refactor(domain): simplificar Patente_83KI

--- Service/Entidades/Patente_83KI.cs
+++ Service/Entidades/Patente_83KI.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace Service.Entidades
@@ -12,19 +11,8 @@ namespace Service.Entidades
         {
         }
 
-        public override void Agregar(ComponentePermiso_83KI componente)
-        {
-            throw new InvalidOperationException("Una patente no puede contener otros permisos.");
-        }
-
-        public override void Remover(ComponentePermiso_83KI componente)
-        {
-            throw new InvalidOperationException("Una patente no puede contener otros permisos.");
-        }
-
         public override IEnumerable<Patente_83KI> ObtenerPatentes()
         {
-            //Retorna un IEnumerable que contiene un único elemento: la instancia misma de la clase donde está este método.
             yield return this;
         }
     }
